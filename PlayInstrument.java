f78ffd2848e5099bf0f43216909038d226fa2a21
NatanzMeshaal <natanz_saptoaji_33rpl@student.smktelkom-mlg.sch.id>
2024-09-02 19:16:26 +0700

    Add files via upload

---
+++ PlayInstrument.java
@@ -0,0 +1,14 @@
+//Driver class
+public class PlayInstrument{
+    public static void main(String[] args) {
+        Instrument a = new Instrument();
+        Instrument b = new Instrument("Piano", 200.0);
+        Instrument c = new Instrument("Violin", 150.9);
+
+        a.setType("Guitar");
+        a.setPrice(101.5);
+        a.print();
+        b.print();
+        c.print();
+    }
+}
